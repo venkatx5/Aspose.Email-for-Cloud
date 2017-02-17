3085a221e264c8a97bd8bc481fc3298d854a2a96
SOHAIL <msohailismail@gmail.com>
2017-02-17 22:23:10 +0500

    Added Examples for Ruby Platform

--- Examples/Ruby/Email/get_document_with_format.rb
+++ Examples/Ruby/Email/get_document_with_format.rb
@@ -7,7 +7,7 @@ class Email
 
   def initialize
     #Get App key and App SID from https://cloud.aspose.com
-    AsposeApp.app_key_and_sid("da77c9f5da014d91faf2961ecec2de66", "B01A15E5-1B83-4B9A-8EB3-0F2BFA6AC766")
+    AsposeApp.app_key_and_sid("", "")
     @email_api = EmailApi.new
   end
 
