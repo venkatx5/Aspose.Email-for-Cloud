fedf02290f9797f0f647dbfa84114236a6acb296
SOHAIL <msohailismail@gmail.com>
2017-02-19 21:29:58 +0500

    Updated Ruby Examples to use common data folder

--- Examples/Ruby/Email/get_email_in_specified_format.rb
+++ Examples/Ruby/Email/get_email_in_specified_format.rb
@@ -13,7 +13,7 @@ class Email
 
   def upload_file(file_name)
     @storage_api = StorageApi.new 
-    response = @storage_api.put_create(file_name, File.open("../data/" << file_name,"r") { |io| io.read } )
+    response = @storage_api.put_create(file_name, File.open("../../../data/" << file_name,"r") { |io| io.read } )
   end
 
   # Get email in specified format
--- Examples/Ruby/EmailProperties/get_mail_common_info.rb
+++ Examples/Ruby/EmailProperties/get_mail_common_info.rb
@@ -7,13 +7,13 @@ class EmailProperties
 
   def initialize
     #Get App key and App SID from https://cloud.aspose.com
-    AsposeApp.app_key_and_sid("", "")
+    AsposeApp.app_key_and_sid("da77c9f5da014d91faf2961ecec2de66", "B01A15E5-1B83-4B9A-8EB3-0F2BFA6AC766")
     @email_api = EmailApi.new
   end
 
   def upload_file(file_name)
     @storage_api = StorageApi.new 
-    response = @storage_api.put_create(file_name, File.open("../data/" << file_name,"r") { |io| io.read } )
+    response = @storage_api.put_create(file_name, File.open("../../../data/" << file_name,"r") { |io| io.read } )
   end
 
   # Get mail common info.
